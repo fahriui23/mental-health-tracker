b887810ee03fdbb6daef1d324a8d391f1e18e98f
fahriui23 <trias.fahri@ui.ac.id>
2024-09-25 21:01:01 +0700

    Tutorial

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid  # tambahkan baris ini di paling atas
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
