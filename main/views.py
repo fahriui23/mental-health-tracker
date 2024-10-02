a50892fc478318ce2516e944b4c2aa8043eb0a39
fahriui23 <trias.fahri@ui.ac.id>
2024-10-02 12:52:09 +0700

    tutorial 5

--- main/views.py
+++ main/views.py
@@ -7,6 +7,9 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 import datetime
 
 from django.urls import reverse
@@ -14,7 +17,6 @@ from django.urls import reverse
 
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
 
     default_last_login = request.COOKIES['last_login'] if 'last_login' in request.COOKIES else {
         'last_login': 'Never logged in'
@@ -23,7 +25,6 @@ def show_main(request):
         'name': request.user.username,
         'class': 'PBP B',
         'npm': '2306212096',
-        'mood_entries': mood_entries,
         'last_login': default_last_login,
     }
 
@@ -43,11 +44,11 @@ def create_mood_entry(request):
 
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -80,7 +81,9 @@ def login_user(request):
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
-
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
+      
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
@@ -115,3 +118,19 @@ def delete_mood(request, id):
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
 
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood"))
+    feelings = strip_tags(request.POST.get("feelings"))
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
